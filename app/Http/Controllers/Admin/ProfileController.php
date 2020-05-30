7df61fe64f837848644999435ac510edd5f9244c
EC2 Default User <ec2-user@ip-172-31-43-82.ap-northeast-1.compute.internal>
2020-05-30 14:37:25 +0000

    課題14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// 以下を追記することでProfile Modelが扱えるようになる
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -26,5 +29,22 @@ class ProfileController extends Controller
     public function update()
     {
         return redirect('admin/profile/edit');
+    }
+    
+    public function create_2(Request $request)
+    {
+        // Varidationを行う
+        $this->Validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        
+        // データベースに保存する
+        $news->fill($form);
+        $news->save();
+        
+        // admin/profile/createにリダイレクトする
+        return redirect('admin/profile/create');
     }
 }
